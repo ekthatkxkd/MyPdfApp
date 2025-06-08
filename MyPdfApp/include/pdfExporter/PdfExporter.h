8fa8c3633963e7de11f66209a66131371ad85385
LEEDASOM <ekthatkxkd@naver.com>
2025-06-08 23:10:31 +0900

    [Feat] : Implemented a function to retrieve table information from the cpp section
    1. TemplateTable 내에 선언된 table 정보들을 가지고 있는 property의 값들을
       cpp 단에서 가져오는 기능 구현함.
    
    Close : []

--- MyPdfApp/include/pdfExporter/PdfExporter.h
+++ MyPdfApp/include/pdfExporter/PdfExporter.h
@@ -5,6 +5,18 @@
 #include <QQuickItem>
 #include <QPdfWriter>
 
+struct TableCellData {
+    int startRow, startCol;
+    int rowSpan, colSpan;
+    QString cellText;
+    bool isTextEdit;
+    QColor bgColor;
+    bool isBold;
+    int fontSize;
+    QString alignPosition;
+    bool isVerticalDir;
+};
+
 class PdfExporter : public QObject {
     Q_OBJECT
 public :
@@ -18,7 +30,7 @@ public :
     Q_INVOKABLE bool exportToPdf(QQuickItem *rootItem, const QString &filePath);
 
     const QList<QString> templateObjNames = {"materialItemArea"};
-    const QList<QString> materialObjNames = {"templateTitle"};
+    const QList<QString> materialObjNames = {"templateTitle", "informTable"};
 
     const double mmA4Width = 210.0;  // a4 full size
     const double mmA4Height = 297.0;  // a4 full size
@@ -27,6 +39,10 @@ public :
     const int mmPdfDefaultMargin = 15;
 
 private :
+    TableCellData convertToTableCellData(const QVariantMap &map);
+    QList<TableCellData> convertToRowData(const QVariantList &rowList);
+    QList<QList<TableCellData>> getCellDatas(QQuickItem *tableItem, const QString &propertyName);
+
     QHash<QString, QQuickItem*> getChildItems(QQuickItem *rootItem, const QList<QString> childObjNames);
     QQuickItem* getChildItem(QQuickItem *rootItem, const QString &childObjName);
     QQuickItem* getInnerItem(QQuickItem *rootItem, const QString &objNameToFound);
@@ -35,11 +51,13 @@ private :
     void setFont(QPainter &painter, int fontSize = 12, bool isBold = false);
 
     QRectF drawTemplateTitle(QPainter &painter, QQuickItem *textItem);
+    QRectF drawTemplateTable(QPainter &painter, QPdfWriter &pdfWriter, QQuickItem *tableItem, QPointF &cursorPoint);
 
-    void drawMaterialTemplate(QPainter &painter, QQuickItem *rootItem);
+    void drawMaterialTemplate(QPainter &painter, QPdfWriter &pdfWriter, QQuickItem *rootItem);
 
     void testDrawFullRectWithRect(QPainter &painter);
     void testDrawFullRectWithWindow(QPainter &painter);
+    void testOutputCellDatas(const TableCellData &cellData);
 
     QSizeF pxContentsFullSize{0, 0};  // A4 전체 pixel size 가 아닌 margin 이 반영된 pixel size.
 };
--- MyPdfApp/src/pdfExporter/PdfExporter.cpp
+++ MyPdfApp/src/pdfExporter/PdfExporter.cpp
@@ -38,6 +38,72 @@ void PdfExporter::setFont(QPainter &painter, int fontSize, bool isBold) {
     painter.setPen(QPen(Qt::black, 2));
 }
 
+TableCellData PdfExporter::convertToTableCellData(const QVariantMap &map) {
+    TableCellData data;
+
+    data.startRow = map.value("startRow", 0).toInt();
+    data.startCol = map.value("startCol", 0).toInt();
+    data.rowSpan = map.value("rowSpan", 1).toInt();
+    data.colSpan = map.value("colSpan", 1).toInt();
+    data.cellText = map.value("cellText", "").toString();
+    data.isTextEdit = map.value("isTextEdit", false).toBool();
+    data.bgColor = QColor(map.value("bgColor", "white").toString());
+    data.isBold = map.value("isBold", false).toBool();
+    data.fontSize = map.value("fontSize", 12).toInt();
+    data.alignPosition = map.value("alignPosition", "right").toString();
+    data.isVerticalDir = map.value("isVerticalDir", false).toBool();
+
+    return data;
+}
+
+QList<TableCellData> PdfExporter::convertToRowData(const QVariantList &rowList) {
+    QList<TableCellData> cellDatas;
+
+    for (const QVariant &cellVariant : rowList) {
+        if (cellVariant.canConvert<QVariantMap>()) {
+            QVariantMap cellMap = cellVariant.toMap();
+
+            TableCellData cellData = convertToTableCellData(cellMap);
+
+            cellDatas.append(cellData);
+        }
+    }
+
+    return cellDatas;
+}
+
+QList<QList<TableCellData>> PdfExporter::getCellDatas(QQuickItem *tableItem, const QString &propertyName) {
+    QList<QList<TableCellData>> cellDatas;
+
+    QVariantList varCellDatas = tableItem->property(propertyName.toLocal8Bit().data()).toList();
+
+    if (!varCellDatas.isEmpty()) {
+        const QVariant &firstElement = varCellDatas.first();
+
+        bool isDoubleArray = firstElement.canConvert<QVariantList>();
+
+        if (isDoubleArray) {
+            // innerData 와 같이 이차 배열의 cellData 를 읽어오기
+            for (int i = 0; i < varCellDatas.size(); i++) {
+                const QVariant &rowVariant = varCellDatas[i];
+
+                QVariantList rowList = rowVariant.toList();
+                QList<TableCellData> rowData = convertToRowData(rowList);
+
+                cellDatas.append(rowData);
+            }
+
+        } else {
+            // headerData 와 같이 일차 배열의 cellData 를 읽어오기
+            QList<TableCellData> rowData = convertToRowData(varCellDatas);
+
+            cellDatas.append(rowData);
+        }
+    }
+
+    return cellDatas;
+}
+
 QHash<QString, QQuickItem*> PdfExporter::getChildItems(QQuickItem *rootItem, const QList<QString> childObjNames) {
     QHash<QString, QQuickItem*> childItems;
 
@@ -114,8 +180,50 @@ QRectF PdfExporter::drawTemplateTitle(QPainter &painter, QQuickItem *textItem) {
     return titleArea;
 }
 
-void PdfExporter::drawMaterialTemplate(QPainter &painter, QQuickItem *rootItem) {
+QRectF PdfExporter::drawTemplateTable(QPainter &painter, QPdfWriter &pdfWriter, QQuickItem *tableItem, QPointF &cursorPoint) {
+    QRectF tableArea(QPointF(0, 0), QSizeF(0, 0));
+
+    int tableRowCount = tableItem->property("dividedRowCount").toInt();
+    int innerTableColCount = tableItem->property("dividedInnerColCount").toInt();
+
+
+    QString tableTitle = tableItem->property("tableTextValue").toString();
+
+    QList<QList<TableCellData>> headerData = getCellDatas(tableItem, "headerData");
+    QList<QList<TableCellData>> innerData = getCellDatas(tableItem, "innerDatas");
+    QList<QList<TableCellData>> footerData = getCellDatas(tableItem, "footerData");
+
+    std::vector<double> pxCellWidths(tableRowCount, 0);
+    std::vector<double> pxCellHeights(innerTableColCount, 0);
+
+
+
+    //////// innerTable 그리기
+    ///
+    ///
+
+    // 1. pxCellWidths, pxCellHeights 객체에 값을 initializing 하기
+    // 2. n 번째 col 에 위치한 cell data 들을 QRectF boundingBox 계산해서
+    //    가장 큰 height 값을 구하고 이 값을 pxCellHeights 객체의 n 번째 col 에 해당하는 index 에 값 변경하기.
+
+    // * newPage 고려하기
+
+    ///
+    ///
+    ////////
+
+
+
+
+
+
+    return tableArea;
+}
+
+void PdfExporter::drawMaterialTemplate(QPainter &painter, QPdfWriter &pdfWriter, QQuickItem *rootItem) {
     QPointF cursorPoint(0, 0);
+
+    std::vector<QRectF> childItemRect(materialObjNames.length());
 
     QHash<QString, QQuickItem*> templateQuickItems = getChildItems(rootItem, materialObjNames);
 
@@ -128,14 +236,22 @@ void PdfExporter::drawMaterialTemplate(QPainter &painter, QQuickItem *rootItem) 
     ///
     ///
     if (templateQuickItems[materialObjNames[0]] != nullptr) {
-        setFont(painter, 20, true);
-        drawTemplateTitle(painter, templateQuickItems[materialObjNames[0]]);
+        childItemRect[0] = drawTemplateTitle(painter, templateQuickItems[materialObjNames[0]]);
     }
+    setFont(painter);
     ///
     ///
     ////////
 
-    setFont(painter);
+    //////// informTable 그리기
+    ///
+    ///
+    if (templateQuickItems[materialObjNames[1]] != nullptr) {
+        childItemRect[1] = drawTemplateTable(painter, pdfWriter, templateQuickItems[materialObjNames[1]], cursorPoint);
+    }
+    ///
+    ///
+    ////////
 }
 
 bool PdfExporter::exportToPdf(QQuickItem *rootItem, const QString &filePath) {
@@ -155,7 +271,7 @@ bool PdfExporter::exportToPdf(QQuickItem *rootItem, const QString &filePath) {
     setFont(painter);
 
     if (rootItem->objectName() == templateObjNames[0]) {
-        drawMaterialTemplate(painter, rootItem);
+        drawMaterialTemplate(painter, pdfWriter, rootItem);
     } else {
 
     }
@@ -188,4 +304,18 @@ void PdfExporter::testDrawFullRectWithWindow(QPainter &painter) {
     qDebug() << "[LLDDSS] fullArea y : " << fullArea.y();
     qDebug() << "[LLDDSS] fullArea width : " << fullArea.width();
     qDebug() << "[LLDDSS] fullArea height : " << fullArea.height();
+}
+
+void PdfExporter::testOutputCellDatas(const TableCellData &cellData) {
+    qDebug() << "[LLDDSS] --------[CellDatas]--------";
+    qDebug() << "[LLDDSS] startRow : " << cellData.startRow << ", startCol : " << cellData.startCol;
+    qDebug() << "[LLDDSS] rowSpan : " << cellData.rowSpan << ", colSpan : " << cellData.colSpan;
+    qDebug() << "[LLDDSS] cellText : " << cellData.cellText;
+    qDebug() << "[LLDDSS] isTextEdit : " << cellData.isTextEdit;
+    qDebug() << "[LLDDSS] bgColor : " << cellData.bgColor;
+    qDebug() << "[LLDDSS] isBold : " << cellData.isBold;
+    qDebug() << "[LLDDSS] fontSize : " << cellData.fontSize;
+    qDebug() << "[LLDDSS] alignPosition : " << cellData.alignPosition;
+    qDebug() << "[LLDDSS] isVerticalDir : " << cellData.isVerticalDir;
+    qDebug() << "[LLDDSS] ---------------------------";
 }
